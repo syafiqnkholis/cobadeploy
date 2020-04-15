8527eda2d834615212a5721df789a7580dd51803
syafiqnkholis <syafiqnkh@gmail.com>
2020-04-15 07:31:05 +0700

    Updates

---
+++ precache-manifest.5968a1f0555fb828f1c1d0ac34f5e2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b83e0e8bc1df0942c04890dc8cb994",
+    "url": "/cobadeploy/index.html"
+  },
+  {
+    "revision": "e8d34c257a0d82a4ab91",
+    "url": "/cobadeploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fcab4acd148a587d392",
+    "url": "/cobadeploy/static/js/2.151747d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cobadeploy/static/js/2.151747d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d34c257a0d82a4ab91",
+    "url": "/cobadeploy/static/js/main.0d7912a4.chunk.js"
+  },
+  {
+    "revision": "bdacd33d57b45eb2188a",
+    "url": "/cobadeploy/static/js/runtime-main.a803b722.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cobadeploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
